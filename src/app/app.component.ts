8bc670b1fdb3768da825f9eadeb5871dea2103ed
murage <murage@labskenya.com>
2022-09-05 18:34:04 +0300

    feat: add a demo request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'potato';
+  result: any;
+
+  constructor(private httpClient: HttpClient) {
+    this.demo().subscribe({
+      next: (data) => {
+        this.result = data;
+      },
+      error: (err) => {
+        this.result = err;
+      },
+    });
+  }
+  demo(): Observable<any> {
+    return this.httpClient.get('/api/events.json');
+  }
 }
